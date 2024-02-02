94f73f916b7974e138e70a5b6df6800af349b002
vrncndlr <veronica.j.nadler@gmail.com>
2024-02-02 15:26:18 +0100

    changes to server.js and some structure stuff

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,25 +3,33 @@ const SERVER_PORT = 8000;
 const path = require('path');
 const APP_ROOT_DIR = path.join(__dirname, '..');
 
+/**
+ * Load the configuration files
+ */
+require('dotenv-safe').config({
+    path: path.join(APP_ROOT_DIR, '.env'),
+    example: path.join(APP_ROOT_DIR, '.env.example'),
+});
+
 const express = require('express');
 const app = express();
 //app.use(express.static(path.join(APP_ROOT_DIR, 'public')));
 
+/**
+ * Handle HTTP responses
+ * @type {{json: Function, raw: Function, text: Function, urlencoded: Function}|{json?: *, raw?: *, text?: *, urlencoded?: *}}
+ */
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
-
-
 app.get('/', (req, res) => {
   return res.send('hello from group 16');
 });
 
-
-
-const loginRoute = require('./src/api')
+const loginRoute = require('./api')
 app.use(loginRoute);
 
 /*app.post('/login', async (req, res) => {
