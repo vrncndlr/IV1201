7ae426c05fca64a0450e0d9f5e9743f46b0231db
Elin Blomquist <eblomquisst@gmail.com>
2024-02-06 16:03:07 +0100

    Frontend skeleton with complete views and presenters

---
+++ frontend/src/view/ExperienceView.js
@@ -0,0 +1,10 @@
+export default ExperienceView;
+
+function ExperienceView() {
+
+    return (
+        <select>Expertise</select>,
+        <select>Years of expertise</select>,
+        <button>Submit</button>
+    )
+}
