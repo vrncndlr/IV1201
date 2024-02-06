32ac2c3fa3d25a544e07dea529fbc1b0a82b43be
vrncndlr <veronica.j.nadler@gmail.com>
2024-02-06 15:37:23 +0100

    worked on the login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Login from "./presenter/LoginPresenter"
 import {Authenticate} from './integration/DBCaller'
-import {useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+// Express-based auth server that uses JWT tokens to authenticate users
+// npm i cors bcrypt jsonwebtoken lowdb
 
 function App() {
   function callDB(user){
--- frontend/src/presenter/LoginPresenter.js
+++ frontend/src/presenter/LoginPresenter.js
@@ -1,19 +1,26 @@
 import LoginView from "../view/LoginView";
-
+import React, { useState } from "react";
 export default
 
 function Login(props) {
-    let cred;
-    function loginACB(cred) {
-        let user = cred.username; //code to show object
-        let pass = cred.password; //it needs model?
-        console.log(cred)
-        props.callDB(cred);
-    }
-console.log(cred)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+      let cred;
+      let user;
+      let pass;
+          function loginACB(cred) {
+              user = cred.username; //code to show object
+              pass = cred.password; //it needs model?
+              console.log(cred)
+              props.callDB(cred);
+          }
+
     return (
         <LoginView
             onLogin={loginACB}
+            username={user}
+            password={pass}
         />
     )
 }
