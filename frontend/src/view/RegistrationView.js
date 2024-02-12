d892dee0bff569fd56604d09455be298a738f872
vrncndlr <veronica.j.nadler@gmail.com>
2024-02-12 10:17:24 +0100

    better form handling

--- frontend/src/view/RegistrationView.js
+++ frontend/src/view/RegistrationView.js
@@ -1,214 +1,93 @@
 import React, {useState} from "react";
 import '../styling/forms.css'
 import {Link} from 'react-router-dom';
+import {useFormik} from 'formik'
 
 function RegistrationView(props) {
-    const [formInput, setFormInput] = useState({
-        firstname:"",
-        lastname:"",
-        username:"",
-        pid:"",
-        email: "",
-        password: "",
-        confirmPassword: "",
-    });
-
-    const [formError, setFormError] = useState({
-        firstname:"",
-        lastname:"",
-        username:"",
-        pid:"",
-        email: "",
-        password: "",
-        confirmPassword: "",
-    });
-
-    const handleUserInput = (name, value) => {
-        setFormInput({
-            ...formInput,
-            [name]: value,
-        });
-    };
-
-    const validateFormInput = (event) => {
-        event.preventDefault();
-        let inputError = {
-            firstname:"",
-            lastname:"",
-            username:"",
-            pid:"",
-            email: "",
-            password: "",
-            confirmPassword: "",
-        };
-
-        if (!formInput.email && !formInput.password) {
-            setFormError({
-                ...inputError,
-                firstname:"Enter your first name",
-                lastname:"Enter your last name",
-                username:"Enter a username",
-                pid:"Enter your personal identification number, 10 digits",
-                email: "Enter valid email address",
-                password: "Password should not be empty",
-                confirmPassword: "Password and confirm password should be same",
-            });
-            return
+    const formik = useFormik({
+        // Manage form state
+        initialValues: {
+            firstname: '',
+            lastname: '',
+            pid: '',
+            email: '',
+            username: '',
+            password: '',
+            confirmPassword: ''
+        },
+        // Submit form data
+        onSubmit: values => {
+            //TODO: call the api with the data as the payload
+        },
+        // Validate
+        validate: values => {
+            let errors = {}
+            if(!values.firstname){errors.firstname = "Required"}
+            if(!values.lastname){errors.lastname = "Required" }
+            if(!values.pid){errors.pid = "Required"}
+            if(!values.email){errors.email = "Required"}else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+                errors.email = "Invalid email format";}
+            if(!values.username){errors.username = "Required"}
+            if(!values.password){errors.password = "Required"}
+            if(!values.confirmPassword){errors.confirmPassword = "Required"}else if(values.confirmPassword !== values.password){
+                errors.confirmPassword = "Must be the same as password"}
+            return errors
         }
-        if (!formInput.firstname) {
-            setFormError({
-                ...inputError,
-                firstname:"Enter your first name",
-            });
-            return
-        }
-        if (!formInput.lastname) {
-            setFormError({
-                ...inputError,
-                lastname:"Enter your last name",
-            });
-            return
-        }
-        if (!formInput.username) {
-            setFormError({
-                ...inputError,
-                username:"Enter a username",
-            });
-            return
-        }
-
-        if (!formInput.email) {
-            setFormError({
-                ...inputError,
-                email: "Enter valid email address",
-            });
-            return
-        }
-
-        if (!formInput.pid) {
-            setFormError({
-                ...inputError,
-                email: "Enter valid email address",
-            });
-            return
-        }
-
-        if (formInput.confirmPassword !== formInput.password) {
-            setFormError({
-                ...inputError,
-                confirmPassword: "Password and confirm password should be same",
-            });
-            return;
-        }
-
-        if (!formInput.password) {
-            setFormError({
-                ...inputError,
-                pid:"Enter your personal identification number, 10 digits",
-            });
-            return
-        }
-
-        setFormError(inputError);
-    };
-    /* gamla
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!firstname || !lastname ||
-            !email || !pid || !username|| !password) {
-            setError('Please fill in all fields');
-            return;
-        }
-        // Your sign-in logic here
-    };
-     */
-    //TODO Missing fields check -> error message. <FORM/>?
-
-    return (
+    })
+    return(
         <div className={"mainContainer"}>
-            <h1>Sign up</h1>
-            <form onSubmit={validateFormInput}>
-                {/*First name*/}
-                <p className={"label"}>First Name</p>
-                <input type="text"
-                       value={formInput.firstname}
-                       onChange={({ target }) => {
-                           handleUserInput(target.name, target.value);}}
-                       name={"firstname"}
-                       placeholder="First Name"
-                       className={"registerField"}/>
-                <p className={"error-message"}>{formError.firstname}</p>
-                <p className={"label"}>Last Name</p>
-                {/*Last name*/}
-                <input type="text"
-                       value={formInput.lastname}
-                       onChange={({ target }) => {
-                           handleUserInput(target.name, target.value);}}
-                       name={"lastname"}
-                       placeholder="Last Name"
-                       className={"registerField"}/>
-                <p className={"error-message"}>{formError.lastname}</p>
-                {/*Username*/}
-                <p className={"label"}>Username</p>
-                <input type="text"
-                       value={formInput.username}
-                       onChange={({ target }) => {
-                           handleUserInput(target.name, target.value);}}
-                       name={"username"}
-                       placeholder="Username"
-                       className={"registerField"}/>
-                <p className={"error-message"}>{formError.username}</p>
-                {/*Email*/}
-                <p className={"label"}>Email</p>
-                <input value={formInput.email}
-                       type="email"
-                       onChange={({ target }) => {
-                           handleUserInput(target.name, target.value);}}
-                       name={"email"}
-                       placeholder="Email address"
-                       className={"registerField"}/>
-                <p className={"error-message"}>{formError.email}</p>
-                {/*Personal id nr*/}
-                <p className={"label"}>Personal ID number</p>
-                <input type="number"
-                       value={formInput.pid}
-                       onChange={({ target }) => {
-                           handleUserInput(target.name, target.value);}}
-                       name={"pid"}
-                       placeholder="Personal Identity Number"
-                       className={"registerField"}/>
-                <p className={"error-message"}>{formError.pid}</p>
-                {/*Password*/}
-                <p className={"label"}>Password</p>
-                <input type="password"
-                       value={formInput.password}
-                       onChange={({ target }) => {
-                           handleUserInput(target.name, target.value);}}
-                       name="password"
-                       placeholder="Password"
-                       className={"registerField"}/>
-                <p className={"error-message"}>{formError.password}</p>
-                {/*Confirm password*/}
-                <p className={"label"}>Confirm your password</p>
-                <input type="password"
-                       value={formInput.confirmPassword}
-                       onChange={({ target }) => {
-                           handleUserInput(target.name, target.value);}}
-                       name="confirmPassword" placeholder="Confirm password"
-                       className={"registerField"}/>
-                <p className={"error-message"}>{formError.confirmPassword}</p>
-
-
-                <p>Already a member? <Link to={"/login"}>Login</Link></p>
-                <button>Submit & register</button>
-                {/*<div className={"errorContainer"}>
-                    {error && <div style={{color: 'red'}}>{error}</div>}
-                </div>*/}
-            </form>
-
+            <h1>Register here</h1>
+            <div className={"inputContainer"}>
+                <form onSubmit={formik.handleSubmit}>
+                    <div className={"nameFields"}>
+                        <div className={"inputGroup"}>
+                            <label htmlFor={"firstname"}>First name</label>
+                            <input type={"text"} id={"firstname"} name={"firstname"} onChange={formik.handleChange} value={formik.values.firstname}/>
+                            {formik.errors.firstname ? <div className={"error-message"}>{formik.errors.firstname}</div> : null}
+                        </div>
+                        <div className="inputGroup">
+                            <label htmlFor={"lastname"}>Last name</label>
+                            <input type={"text"} id={"lastname"} name={"lastname"} onChange={formik.handleChange} value={formik.values.lastname}/>
+                            {formik.errors.lastname ? <div className={"error-message"}>{formik.errors.lastname}</div> : null}
+                        </div>
+                    </div>
+                    <div className="inputGroup">
+                        <label htmlFor={"pid"}>Personal Identification number</label>
+                        <input type={"number"} id={"pid"} name={"pid"} onChange={formik.handleChange} value={formik.values.pid}/>
+                        {formik.errors.pid ? <div className={"error-message"}>{formik.errors.pid}</div> : null}
+                    </div>
+                    <div className="inputGroup">
+                        <label htmlFor={"email"}>Email</label>
+                        <input type={"email"} id={"email"} name={"email"} onChange={formik.handleChange}
+                               value={formik.values.email}/>
+                        {formik.errors.email ?  <div className={"error-message"}>{formik.errors.email}</div> : null}
+                    </div>
+                    <div className="inputGroup">
+                        <label htmlFor={"username"}>Username</label>
+                        <input type={"text"} id={"username"} name={"username"}
+                               onChange={formik.handleChange}
+                               value={formik.values.username}/>
+                        {formik.errors.username ? <div className={"error-message"}>{formik.errors.username}</div> : null}
+                    </div>
+                    <div className="inputGroup">
+                        <label htmlFor={"password"}>Password</label>
+                        <input type={"password"} id={"password"} name={"password"}
+                               onChange={formik.handleChange}
+                               value={formik.values.password}/>
+                        {formik.errors.password ? <div className={"error-message"}>{formik.errors.password}</div> : null}
+                    </div>
+                    <div className="inputGroup">
+                        <label htmlFor={"password"}>Confirm password</label>
+                        <input type={"password"} id={"confirmPassword"} name={"confirmPassword"}
+                               onChange={formik.handleChange} value={formik.values.confirmPassword}/>
+                        {formik.errors.confirmPassword ?  <div className={"error-message"}>{formik.errors.confirmPassword}</div> : null}
+                    </div>
+                <button type={"submit"}>Register</button>
+                </form>
+            </div>
+            <p>Already have an account? <Link to={"/login"}>Sign in</Link></p>
         </div>
     )
 }
 
 export default RegistrationView;
-
