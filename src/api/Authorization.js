1ba28317063f60587c3dc3722a24545c72f57ae6
Elin Blomquist <eblomquisst@gmail.com>
2024-02-20 16:21:44 +0100

    deploy setup

--- src/api/Authorization.js
+++ src/api/Authorization.js
@@ -42,7 +42,7 @@ class Authorization{
 
     const JWTToken = jwt.sign(
       {id:user.id, user:user.username},
-        1234,
+        '1234',
       //process.env.JWT_SECRET,
       sessionCookie,
     );
