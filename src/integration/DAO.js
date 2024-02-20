ea1a6ecc6afce010495d74e5a7f3de0b021cd8f5
Elin Blomquist <eblomquisst@gmail.com>
2024-02-20 15:56:39 +0100

    deploy setup

--- src/integration/DAO.js
+++ src/integration/DAO.js
@@ -15,11 +15,11 @@ class DAO {
   constructor() {
     const {Pool, Client} = require('pg');
     this.pool = new Pool({
-      user: process.env.USER,
-      host: process.env.HOST,
-      database: process.env.NAME,
-      password: process.env.PASSWD,
-      port: process.env.PORT
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      port: process.env.PG_PORT
     })
   }
 
