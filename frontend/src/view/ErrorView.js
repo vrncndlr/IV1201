0c627ba249da6cd1b218417bd0bf8cb223069a55
Elin Blomquist <eblomquisst@gmail.com>
2024-02-20 14:20:20 +0100

    deploy setup

--- frontend/src/view/ErrorView.js
+++
@@ -1,9 +0,0 @@
-import {Link} from 'react-router-dom';
-/**
- * 
- * @returns server error message
- */
-export default function ErrorView(){
-  return <><div>server error</div>
-    <p>Already have an account? <Link to={"/login"}>Sign in</Link></p></>
-}
