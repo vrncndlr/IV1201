c4039c4feed5d94e0c8d88fa083566226b91ad52
Elin Blomquist <eblomquisst@gmail.com>
2024-02-22 19:15:13 +0100

    deploy setup

--- src/server.js
+++ src/server.js
@@ -31,11 +31,11 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 
- // CORS: put in root URL without / in the first header below. https://archdes-frontend-5528c891010d.herokuapp.com/
+ // CORS: put in root URL without / in the first header below. https://archdes-frontend-5528c891010d.herokuapp.com
 // Has to include http://
 // CORS configuration
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://archdes-frontend-5528c891010d.herokuapp.com", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://archdes-frontend-5528c891010d.herokuapp.com*", "http://localhost:3000*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
