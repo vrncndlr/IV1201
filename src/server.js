31db16c782ca1047fdf96d26c5da43bda800dc77
Elin Blomquist <eblomquisst@gmail.com>
2024-02-22 20:34:40 +0100

    deploy setup

--- src/server.js
+++ src/server.js
@@ -35,9 +35,7 @@ app.use(cookieParser());
 // Has to include http://
 // CORS configuration
 app.use((req, res, next) => {
-    process.env.NODE_ENV === 'production' ?
-    res.header("Access-Control-Allow-Origin", "https://archdes-frontend-5528c891010d.herokuapp.com") :
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    (process.env.NODE_ENV === 'production')?res.header("Access-Control-Allow-Origin", "https://archdes-frontend-5528c891010d.herokuapp.com"):res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
