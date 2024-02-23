99075dfb8a3f5d56f3e2beaf14fdb905a34bd8dc
Elin Blomquist <eblomquisst@gmail.com>
2024-02-23 11:37:28 +0100

    deploy setup

--- src/server.js
+++ src/server.js
@@ -36,7 +36,13 @@ app.use(cookieParser());
 // CORS configuration
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", ["https://archdes-frontend-5528c891010d.herokuapp.com", "http://localhost:3000"]);
+    const allowedOrigins=["https://archdes-frontend-5528c891010d.herokuapp.com", "http://localhost:3000"]
+    if(allowedOrigins.indexOf(req.baseUrl) === 0) {
+        res.header("Access-Control-Allow-Origin", "https://archdes-frontend-5528c891010d.herokuapp.com");
+    } else if (allowedOrigins.indexOf(req.baseUrl) === 1){
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    } else (console.log("Origin denied"))
+    //res.header("Access-Control-Allow-Origin", "https://archdes-frontend-5528c891010d.herokuapp.com");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
