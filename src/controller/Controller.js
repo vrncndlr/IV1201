77f865dd9e4f1c64358592d5e9500f67d30b2332
Syncopated90 <ehne@kth.se>
2024-03-08 09:03:33 +0100

    moved transaction handling back to controller from DAO

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -19,8 +19,20 @@ class Controller {
      * @returns
      */
     async updateUserDataByEmailCode(userdata) {
-        return await this.dao.updateUserDataByEmailCode(userdata);
-    }
+      const connection = await this.dao.getConnection();
+      try {
+          await connection.query('BEGIN')
+          const result = await this.dao.updateUserDataByEmailCode(connection, userdata)
+          await connection.query('COMMIT')
+          return result;
+      } catch (e) {
+          await connection.query('ROLLBACK')
+          console.error(e);
+          throw new Error("database error")
+      } finally {
+          connection.release()
+      }
+  }
 
     /**
      * Calls the database layer for login and returns the result.
@@ -30,20 +42,30 @@ class Controller {
      * @returns JSON object {
      }
      */
-    async login(username, password) {
-        const hashedpassword = await this.dao.getLoginUserData(username);
-        //console.log("login in controller")
-        //console.log(hashedpassword)
-        if (hashedpassword[0] == undefined)
-            return undefined;
-        const bool = await this.crypt.checkPassword(password, hashedpassword[0].password);
-
-        if (bool) {
-            return await this.dao.getUser(hashedpassword[0].person_id);
-        }
-        return undefined;
-
-        //return await this.dao.login(username, password);
+     async login(username, password) {
+      const connection = await this.dao.getConnection();
+      try {
+          await connection.query('BEGIN')
+          const hashedpassword = await this.dao.getLoginUserData(connection, username);
+          if (hashedpassword[0] == undefined) {
+              await connection.query('COMMIT')
+              return undefined;
+          }
+          const bool = await this.crypt.checkPassword(password, hashedpassword[0].password);
+          if (bool) {
+              const result = await this.dao.getUser(connection, hashedpassword[0].person_id);
+              await connection.query('COMMIT')
+              return result;
+          }
+          await connection.query('COMMIT')
+          return undefined;
+      } catch (e) {
+          await connection.query('ROLLBACK')
+          console.error(e);
+          throw new Error("database error")
+      } finally {
+          connection.release()
+      }
     }
     /**
      * Calls the database layer with register api function and returns a boolean
@@ -55,16 +77,22 @@ class Controller {
      * @param password
      * @param username
      * @returns true if registration successful and false if not {Promise<boolean>}*/
-    async register(firstname, lastname, pid, email, username, password) {
-        try {
-            const hash = await this.crypt.generateCryptPassword(password);
-            await this.dao.register(firstname, lastname, pid, email, username, hash);
-            return true;
-        } catch (error) {
-            console.error('Error registering user:', error);
-            return false;
-        }
-    }
+    async register(firstname, lastname, pid, email, password, username) {
+      const connection = await this.dao.getConnection();
+      try {
+          await connection.query('BEGIN')
+          const hash = await this.crypt.generateCryptPassword(password);
+          await this.dao.register(connection, firstname, lastname, pid, email, hash, username);
+          await connection.query('COMMIT')
+          return true;
+      } catch (e) {
+          await connection.query('ROLLBACK')
+          console.error('Error registering user:', error);
+          return false;
+      } finally {
+          connection.release()
+      }
+  }
     /**
      * Checks if a user with the supplied email exists in database and is missing username.
      * If so, sends out a restoration code by email and stores it in the database.
@@ -72,23 +100,38 @@ class Controller {
      * @returns true if restoration code was succesfully sent, otherwise false.
      */
     async restoreAccountByEmail(email) {
-        const exists = await this.dao.checkUserEmail(email);
-
-        if (exists == undefined)
-            return false;
-        if (exists != undefined && exists.username) {
-            return false;
-        }
-        if (exists != undefined && exists.person_id) {
-            const mailer = new Email();
-            const [messageSent, accountRestoreCode] = await mailer.sendAccountRestoreMail(exists.email)
-            console.log(messageSent + " " + accountRestoreCode)
-            await this.dao.storeAccountRestoreCode(exists.person_id, accountRestoreCode);
-            return messageSent;
-        }
-        else
-            return false;
-    }
+      const connection = await this.dao.getConnection();
+      try {
+          await connection.query('BEGIN')
+          const exists = await this.dao.checkUserEmail(connection, email);
+          if (exists == undefined) {
+              await connection.query('COMMIT')
+              return false;
+          }
+          if (exists != undefined && exists.username) {
+              await connection.query('COMMIT')
+              return false;
+          }
+          if (exists != undefined && exists.person_id) {
+              const mailer = new Email();
+              const [messageSent, accountRestoreCode] = await mailer.sendAccountRestoreMail(exists.email)
+              console.log(messageSent + " " + accountRestoreCode)
+              await this.dao.storeAccountRestoreCode(connection, exists.person_id, accountRestoreCode);
+              await connection.query('COMMIT')
+              return messageSent;
+          }
+          else {
+              await connection.query('COMMIT')
+              return false;
+          }
+      } catch (e) {
+          await connection.query('ROLLBACK')
+          console.error(e);
+          throw new Error("database error")
+      } finally {
+          connection.release()
+      }
+  }
 
     /**
      * Updates the users personal information
@@ -100,42 +143,126 @@ class Controller {
      * @returns {Promise<void>}
      */
     async update(person_id, name, surname, pnr, email) {
-        await this.dao.updateUserInfo(person_id, name, surname, pnr, email);
-    }
+      const connection = await this.dao.getConnection();
+      try {
+          await connection.query('BEGIN')
+          const result = await this.dao.updateUserInfo(connection, person_id, name, surname, pnr, email)
+          await connection.query('COMMIT')
+          return result;
+      } catch (e) {
+          await connection.query('ROLLBACK')
+          console.error(e);
+          throw new Error("database error")
+      } finally {
+          connection.release()
+      }
+  }
 
     /**
      * Fetches all rows from the table competence in the database
      * @returns {Promise<*>}
      */
     async fetch() {
-        return await this.dao.getAllFromCompetences();
-    }
+      const connection = await this.dao.getConnection();
+      try {
+          await connection.query('BEGIN')
+          const result = await this.dao.getAllFromCompetences(connection);
+          await connection.query('COMMIT')
+          return result;
+      } catch (e) {
+          await connection.query('ROLLBACK')
+          console.error(e);
+          throw new Error("database error")
+      } finally {
+          connection.release()
+      }
+  }
 
     /**
      * Gets all status from databaseHandler and forwards to API
      * @returns {Promise<*|undefined>}
      */
     async setCompetence(person_id, competence_id, monthsOfExperience) {
-        const yearsOfExperience = monthsOfExperience / 12;
-        return await this.dao.createCompetenceProfile(person_id, competence_id, yearsOfExperience);
-    }
+      const connection = await this.dao.getConnection();
+      const yearsOfExperience = monthsOfExperience / 12;
+      try {
+          await connection.query('BEGIN')
+          const result = await this.dao.createCompetenceProfile(connection, person_id, competence_id, yearsOfExperience);
+          await connection.query('COMMIT')
+          return result;
+      } catch (e) {
+          await connection.query('ROLLBACK')
+          console.error(e);
+          throw new Error("database error")
+      } finally {
+          connection.release()
+      }
+  }
     /**
      * Calls the database layer with availability api function
      * @returns {Promise<*|undefined>}
      */
     async setAvailability(person_id, from_date, to_date) {
-        return await this.dao.createAvailability(person_id, from_date, to_date);
+      const connection = await this.dao.getConnection();
+      try {
+          await connection.query('BEGIN')
+          const result = await this.dao.createAvailability(connection, person_id, from_date, to_date);
+          await connection.query('COMMIT')
+          return result;
+      } catch (e) {
+          await connection.query('ROLLBACK')
+          console.error(e);
+          throw new Error("database error")
+      } finally {
+          connection.release()
+      }
+  }
+  async fetchApplicants() {
+    const connection = await this.dao.getConnection();
+    try {
+        await connection.query('BEGIN')
+        const result = await this.dao.getAllStatus(connection)
+        await connection.query('COMMIT')
+        return result;
+    } catch (e) {
+        await connection.query('ROLLBACK')
+        console.error(e);
+        throw new Error("database error")
+    } finally {
+        connection.release()
     }
-    async fetchApplicants() {
-        return await this.dao.getAllStatus();
-    }
+}
 
-    async getUserCompetences(person_id) {
-        return await this.dao.getUserCompetenceProfile(person_id);
-    }
-    async getUserAvailabilities(person_id) {
-        return await this.dao.getUserAvailability(person_id);
-    }
+async getUserCompetences(person_id) {
+  const connection = await this.dao.getConnection();
+  try {
+      await connection.query('BEGIN')
+      const result = await this.dao.getUserCompetenceProfile(connection, person_id)
+      await connection.query('COMMIT')
+      return result;
+  } catch (e) {
+      await connection.query('ROLLBACK')
+      console.error(e);
+      throw new Error("database error")
+  } finally {
+      connection.release()
+  }
+}
+async getUserAvailabilities(person_id) {
+  const connection = await this.dao.getConnection();
+  try {
+      await connection.query('BEGIN')
+      const result = await this.dao.getUserAvailability(connection, person_id)
+      await connection.query('COMMIT')
+      return result;
+  } catch (e) {
+      await connection.query('ROLLBACK')
+      console.error(e);
+      throw new Error("database error")
+  } finally {
+      connection.release()
+  }
+}
 
     /**
  * Writes to logfile.
